b355ccc1106f819942863ff256f8dcb42857c7e9
3bdallahnasser <98500420+3bdallahnasser@users.noreply.github.com>
2023-07-13 05:59:51 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+
+   
+
+function leyo(){
+    var user = ["Oscar Wilde"," J.K. Rowling","Elbert Hubbard","Martin Luther King Jr","Andre Gide","Friedrich Nietzsche"];
+    var tesst = ["“Be yourself; everyone else is already taken.”",
+    "“If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.”",
+    "“A friend is someone who knows all about you and still loves you.”",
+    "“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.”","“It is better to be hated for what you are than to be loved for what you are not.”",
+    "“Without music, life would be a mistake.”"];
+            var foooer = Math.floor( Math.random() * user.length);
+           document.getElementById("quit").innerHTML=  tesst[foooer]  ;
+           document.getElementById("quiq").innerHTML=  user[foooer]  ;
+           
+}
+
+
+
+
+
